03d82615235d579bf22e9371c59c53cec8e8f1c8
keakalamudo <kelo.akalamudo@gmail.com>
2014-07-28 15:13:49 -0700

    directory change

---
+++ JS/datastructures/binary-search-tree/binary-search-tree.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Kaymuda on 7/28/14.
+ */
+/**
+ * This is a binary search tree implementation in JavaScript.
+ * This implementation does not contain any duplication.
+ *
+ * The binary search tree is a fundamental data structure in Computer Science.
+ *
+ */
