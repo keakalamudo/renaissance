68e7add384ea4e4e82ddcda48e9c150dcf4e2a06
keakalamudo <kelo.akalamudo@gmail.com>
2014-07-28 14:59:15 -0700

    webstrom cleanup

---
+++ JS/controlstruc.js
@@ -0,0 +1,8 @@
+var name = "Kittens";
+if (name == "Puppies"){
+  name+= "!";
+}else if (name =="Kittens"){
+  name += "!!";
+} else {
+  name = "!" + name;
+}
--- JS/datasturctures/binary-search-tree.js
+++ JS/datasturctures/binary-search-tree.js
@@ -3,6 +3,7 @@
  */
 /**
  * This is a binary search tree implementation in JavaScript.
+ * This implementation does not contain any duplication. 
  *
  * The binary search tree is a fundamental data structure in Computer Science.
  *
