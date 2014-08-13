edaab73d9c9c2ca98d63a0bf87f9b47073d74741
keakalamudo <kelo.akalamudo@gmail.com>
2014-08-12 22:47:41 -0700

    partition function added + completed

--- JS/algorithms/sorting/quicksort/quicksort.js
+++ JS/algorithms/sorting/quicksort/quicksort.js
@@ -38,3 +38,24 @@ function swap (items, firstindex, secondindex){
     items[secondindex] = temp;
 
 }
+
+function partition(items, left, right){
+    var pivot = items[Math.floor((right+left)/2)],
+        i = left,
+        j = right
+
+    while(i<=j){
+        while(items[i] < pivot){
+            i++;
+        }
+        while(items[i] > pivot){
+            j--;
+        }
+        if(i<=j){
+            swap(items,i,j);
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
