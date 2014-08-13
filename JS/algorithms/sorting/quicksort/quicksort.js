a6991ffb71e5919a49a93ef51ab94e51b8ba1f80
keakalamudo <kelo.akalamudo@gmail.com>
2014-08-12 21:33:43 -0700

    initial commit

---
+++ JS/algorithms/sorting/quicksort/quicksort.js
@@ -0,0 +1,11 @@
+/**
+ * This is a Quicksort implementation in JavaScript.
+ * Quicksort is a divide and conquer algorithm. It first divides a large array into two smaller sub arrays:
+ * the high elements and the low elements. It can then recursively sort the sub array. 
+ *
+ *
+ *
+ *
+ *
+ *
+ */
