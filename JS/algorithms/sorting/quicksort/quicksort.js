f8c2ab49cbe7e9974f55e220c64e10f53bce8424
keakalamudo <kelo.akalamudo@gmail.com>
2014-08-12 22:06:29 -0700

    swap function added

--- JS/algorithms/sorting/quicksort/quicksort.js
+++ JS/algorithms/sorting/quicksort/quicksort.js
@@ -1,11 +1,38 @@
 /**
  * This is a Quicksort implementation in JavaScript.
  * Quicksort is a divide and conquer algorithm. It first divides a large array into two smaller sub arrays:
- * the high elements and the low elements. It can then recursively sort the sub array. 
- *
- *
- *
- *
- *
+ * the high elements and the low elements. It can then recursively sort the sub array.
  *
  */
+
+/**
+ * Details:
+ *
+ * Class        = Sorting Algorithm
+ * Worst Case   = O(n^2)
+ * Average Case = O(n log n)
+ * Best Case    = O(n log n)
+ */
+
+/**
+ * Steps:
+ * -> Pick a pivot in the array.
+ * -> Start pointers at left and right of the array.
+ * -> Reorder pointers to the left or right of the pivot depending on if the value
+ *    of the pointer is larger or smaller than the pivot.
+ *
+ */
+
+/**
+ * Swaps two values in an array.
+ * @param {Array} items The array containing the items.
+ * @param {int} firstIndex Index of first item to swap.
+ * @param {int} secondIndex Index of second item to swap.
+ * @return {void}
+ */
+
+
+function swap (items, firstindex, secondindex){
+    var temp = items[firstindex];
+    
+}
