b0bd9a82ef33dbf0106b1934a10dcfb4d3043395
keakalamudo <kelo.akalamudo@gmail.com>
2014-08-13 11:21:40 -0700

    bubblesort function added

--- JS/algorithms/sorting/bubblesort.js
+++ JS/algorithms/sorting/bubblesort.js
@@ -10,3 +10,25 @@ function swap(items, firstIndex, secondIndex){
     items[firstIndex] = items[secondIndex];
     items[secondIndex] = temp;
 }
+
+/**
+ * A bubble sort implementation in JavaScript. The array
+ * is sorted in-place.
+ * @param {Array} items An array of items to sort.
+ * @return {Array} The sorted array.
+ */
+function bubbleSort(items){
+
+    var len = items.length,
+        i, j, stop;
+
+    for (i=0; i < len; i++){
+        for (j=0, stop=len-i; j < stop; j++){
+            if (items[j] > items[j+1]){
+                swap(items, j, j+1);
+            }
+        }
+    }
+
+    return items;
+}
