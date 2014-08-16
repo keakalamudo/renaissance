63a137713fd3907b11a92205effe9b60559845d9
keakalamudo <kelo.akalamudo@gmail.com>
2014-08-15 23:10:41 -0700

    intial commit

---
+++ JS/algorithms/sorting/merge-sort-recursive.js
@@ -0,0 +1,23 @@
+/**
+ * Merges to arrays in order based on their natural
+ * relationship.
+ * @param {Array} left The first array to merge.
+ * @param {Array} right The second array to merge.
+ * @return {Array} The merged array.
+ */
+
+function merge(left, right){
+    var result  = [],
+        il      = 0,
+        ir      = 0;
+
+    while (il < left.length && ir < right.length){
+        if (left[il] < right[ir]){
+            result.push(left[il++]);
+        } else {
+            result.push(right[ir++]);
+        }
+    }
+
+    return result.concat(left.slice(il)).concat(right.slice(ir));
+}
